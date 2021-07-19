83cd3965210726fec5d8d7dbaa98bc715f6dd7f2
Administrator <Administrator@MANDAR>
2021-07-19 14:58:59 +0530

    Add project files.

---
+++ UserRegistrationForm/Controllers/RegistrationFormController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserRegistrationForm.Models;
+
+namespace UserRegistrationForm.Controllers
+{
+    public class RegistrationFormController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetFormDetails()
+        {
+            Student s = new Student();
+            return View(s);
+        }
+        [HttpPost]
+        public IActionResult GetFromDetails(Student s)
+        {
+            List<Student> list = new List<Student>();
+            list.Add(s);
+            //ViewBag.Students1=s;
+            return View("ShowDetails", list);
+        }
+    }
+}
---
+++ UserRegistrationForm/Models/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRegistrationForm.Models
+{
+    public class Student
+    {
+        [MaxLength(20), MinLength(1)]
+        [Required(ErrorMessage = "FirstName is Mandatory")]
+        public string First_Name { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [MaxLength(20), MinLength(1)]
+        [Required(ErrorMessage = "LastName is Mandatory")]
+        public string Last_Name { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [MaxLength(20), MinLength(1)]
+        [Required(ErrorMessage = "UserName is Mandatory")]
+        public string UserName { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "password in between 5 to 30 characters")]
+        [Required(ErrorMessage = "Passowrd is required")]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(16, ErrorMessage = "Must be between 5 and 50 characters", MinimumLength = 5)]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Must be a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number Required")]
+        [Range(0, 10)]
+        public int Contact { get; set; }
+    }
+}
